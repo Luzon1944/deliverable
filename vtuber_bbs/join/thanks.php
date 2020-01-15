10107f14c5638cb1f180aa96bb542dc43a16ac53
Luzon1944 <rusontou1945@gmail.com>
2020-01-15 21:22:32 +0900

    vtuber_bbs

---
+++ vtuber_bbs/join/thanks.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>会員登録</title>
+	<link rel="stylesheet" href="../stylesheet.css" />
+</head>
+	<body>
+		<header>
+			<div class="header-left">
+				<h1 class="logo">ぶいけー</h1>
+			</div> 
+		</header>
+	<div class="main_margin">
+		<div>
+			<h1>会員登録</h1>
+		</div>
+		<div>
+			<p>ユーザー登録が完了しました</p>
+			<p><a href="../list.php">ログインする</a></p>
+		</div>
+	</div>
+	</body>
+</html>
